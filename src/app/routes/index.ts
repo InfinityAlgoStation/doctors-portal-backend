7b07724296af5b22621a38017f480ff324e33bb2
Nirob Hasan <nirob427417@gmail.com>
2023-08-13 16:22:47 +0600

    test husky

---
+++ src/app/routes/index.ts
@@ -0,0 +1,11 @@
+// import express from 'express';
+
+// const router = express.Router();
+
+// const modulesRoute = [
+
+// ];
+
+// // modulesRoute.map(route => router.use(route.path, route.route));
+
+// export default router;
